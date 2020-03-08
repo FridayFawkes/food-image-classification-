c408294e0ca5d0590fb1bf84301d3813fe8b85ed
FridayFawkes <pedro_miranda2@hotmail.com>
2020-03-08 16:22:00 +0000

    fixed directory hierarchy generated by setup.sh - this fixed the fit problem of not having enough train data

--- setup.sh
+++ setup.sh
@@ -3,12 +3,12 @@
 
 #create the directories for the images
 mkdir -p Data
-mkdir -p Data/Train
-mkdir -p Data/Test
+# mkdir -p Data/Train
+# mkdir -p Data/Test
 
 # create workdirectory - where the program saves the weights, etc
 mkdir -p wdir
 
 # extract the images
-rar x Train.rar Data/Train
-rar x Test.rar Data/Test
+rar x Train.rar Data
+rar x Test.rar Data
